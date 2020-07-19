252eff9b17a453ca228d186bdf02ba0eebf7bc70
eduardoxfurtado <eduardoxfurtado@gmail.com>
2020-07-19 15:36:54 -0400

    new implementration of the test TestScreenAlbum.test_refresh_photolist

--- tests/screens/test_screenAlbum.py
+++ tests/screens/test_screenAlbum.py
@@ -11,16 +11,27 @@ class TestScreenAlbum(GraphicUnitTest):
 
     print(11)
 
+    # def setUp(self):
+    #     self.root = PhotoManager()
+    #     self.root.parent = None
+    #     self.root.canvas = None
+    #     self.root.load_config()
+    #     # self.root.build()
+    #     # self.root.load_config()
+    #
+    #
+    #     self.app = TesterApp()
+    #     # app.text_scale = 10
+    #     # app.padding = 10
+    #
+    #     EventLoop.ensure_window()
+    #     self._win = EventLoop.window
+    #     self.clean_garbage()
+    #
+    #
+    #     super(TestScreenAlbum, self).setUp()
+
     def setUp(self):
-        print(12)
-        self.root = PhotoManager()
-        self.root.parent = None
-        self.root.canvas = None
-        self.root.load_config()
-        # self.root.build()
-        # self.root.load_config()
-
-
         self.app = TesterApp()
         # app.text_scale = 10
         # app.padding = 10
@@ -28,7 +39,6 @@ class TestScreenAlbum(GraphicUnitTest):
         EventLoop.ensure_window()
         self._win = EventLoop.window
         self.clean_garbage()
-
 
         super(TestScreenAlbum, self).setUp()
 
@@ -38,12 +48,9 @@ class TestScreenAlbum(GraphicUnitTest):
         self.advance_frames(5)
 
     def test_refresh_photolist(self):
-        print(1)
         from screens.screenAlbum import ScreenAlbum
-        screen_album = ScreenAlbum(name='album')
-        print(2)
+        screen_album = ScreenAlbum()
         screen_album.refresh_photolist()
-        print(3)
         self.fail()
 
     def test_sort_photos(self):
